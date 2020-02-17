4d8c9ba3e610aa549484489ab4ee9b433209616c
Doğuş Kar <35219098+doguskar@users.noreply.github.com>
2020-02-17 11:18:35 +0300

    Clear Asp.Net Core MVC Project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Kardo20.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
